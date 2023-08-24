2db0efc54d9ee0bb656ac8c9e136d116af7d5e61
heckkers <ambero0@utas.edu.au>
2023-08-24 21:07:42 +1000

    Updated and added movement
    A or LeftArrow turns left
    D or RightArrow turns right
    S does a spin
    Space or W does a jump
    Speed is constantly moving forward, gradually speeding up and capping off at maxSpeed
    
    Added functions:
    Move: moves left or right depending on parameter
    Jump: Jumps up, height dependent on parameter
    Spin: makes the coin jump up and spin 3 times, brief and purely visual with room for further updates
    
    Ensure you are on the MovementTest scene and have the toggle script disabled and the move script enabled.

--- coin/Assets/move.cs
+++ coin/Assets/move.cs
@@ -9,47 +9,94 @@ using UnityEngine.UIElements;
 public class move : MonoBehaviour
 {
     Rigidbody rb;
-    public float speed = 100; //INSPECTOR
-    public float tilt; //INSPECTOR how much to 
     float currentAngle = 0; //current tilt
-    public float maxAngle; //INSPECTOR maximum tilt (right)
-    public float minAngle; //INSPECTOR minimum tilt (left)
-    public float turnAngle; //INSPECTOR angle to turn when tilting (A and D)
+
+    KeyCode left1Input = KeyCode.A;
+    KeyCode left2Input = KeyCode.LeftArrow;
+    KeyCode right1Input = KeyCode.D;
+    KeyCode right2Input = KeyCode.RightArrow;
+    KeyCode up1Input = KeyCode.W;
+    KeyCode up2Input = KeyCode.Space;
+    KeyCode spinInput = KeyCode.S;
+
+    public float speed = 50; //INSPECTOR acceleration speed
+    public float tilt = 1; //INSPECTOR how much to tilt on button press
+    public float maxAngle = 50; //INSPECTOR maximum tilt (right)
+    public float minAngle = -50; //INSPECTOR minimum tilt (left)
+    public float maxSpeed = 12; //INSPECTOR how fast is too fast
+    public float jumpHeight = 700; //INSPECTOR how high to jump
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (rb.velocity.x < maxSpeed && rb.velocity.z < maxSpeed)//(Input.GetKey(KeyCode.W))
         {
             rb.AddRelativeForce(Vector3.forward * speed);
-            //Debug.Log("W");
         }
-        if (Input.GetKey(KeyCode.D))
+        
+        if (Input.GetKey(right1Input) || Input.GetKey(right2Input))
         {
-            currentAngle += tilt;
-            gameObject.transform.Rotate(0, turnAngle * tilt, 0);
+            moveCoin(1); 
         }
-        if (Input.GetKey(KeyCode.A)) //same as D but negative
+        
+        if (Input.GetKey(left1Input)|| Input.GetKey(left2Input)) //same as D but negative
         {
-            currentAngle += -tilt;
-            //Debug.Log("current angle: " + currentAngle + " x :" + transform.rotation.x + " z: " + transform.rotation.z  );
-            gameObject.transform.Rotate(0, turnAngle * -tilt, 0);
+            moveCoin(-1);
+        }
 
-        }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKeyDown(up1Input) || Input.GetKeyDown(up2Input)) //TODO stop player from jumping whilst they are in the air
         {
-            rb.AddRelativeForce(-(Vector3.forward * speed));
-            //Debug.Log("S");
+            Debug.Log("y velocity is " + rb.velocity.y);
+            jumpCoin(jumpHeight);
+        }
+
+        if (Input.GetKeyDown(spinInput))
+        {
+            spinCoin(500);
+        }
+    }
+
+    // Moves the coin, parameters of +1 or -1
+    void moveCoin(int direction)
+    {
+        currentAngle = tilt * direction;
+        gameObject.transform.Rotate(0, tilt * direction, 0);
+    }
+
+    // Called when the user moves left or right to apply the appropriate tilt and balance
+    void tiltCoin()
+    {
+        //TODO update coins tilt to match world
+    }
+
+    // Makes the coin jump upward
+    void jumpCoin(float upwardsForce)
+    {
+        rb.AddRelativeForce(Vector3.up * upwardsForce);
+    }
+
+    // Makes the coin jump and spin
+    void spinCoin(float jumpHeight)
+    {
+        jumpCoin(jumpHeight);
+        Debug.Log("spinning");
+        StartCoroutine(spinAttack());
+    }
+
+    // The spin attack visual coroutine
+    IEnumerator spinAttack()
+    {
+        for (int i = 0; i < 83; i++) //83 is 3 complete spins of 13 degrees
+        {
+            gameObject.transform.Rotate(0, 13, 0); //odd number to offset the coin's spin
+            yield return new WaitForSeconds(.004f); 
         }
     }
 }
