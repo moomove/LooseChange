b3deae0f801c3c31356d9c8d4157bb07f3bd28ed
KaidenUTAS <kaidenolsen64@gmail.com>
2023-10-15 19:03:17 +1100

    Removed assets duplicate

--- Assets/Scripts/notes.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class notes : MonoBehaviour
-{
-    //temp script for keeping brief notes
-    public string[] aaa;
-}
